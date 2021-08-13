5b299eb3bc3e73c349062a11a1b094945b6f2b1e
Tori <aer456987@gmail.com>
2021-08-14 02:04:48 +0800

    加入 swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,26 @@
-$(function() {
+// $(function() {
 
+// });
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  direction: getDirection(),
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
 });
+
+function getDirection() {
+  const windowWidth = window.innerWidth;
+  const direction = window.innerWidth <= 767 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
