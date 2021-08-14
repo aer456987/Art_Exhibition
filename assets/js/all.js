428c33fc4c7e1b7eaecd1ea6f81c7b39b6bdf9c9
Tori <aer456987@gmail.com>
2021-08-14 22:17:38 +0800

    Update 2021-08-14T14:17:36.269Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,34 @@
 "use strict";
 
-$(function () {});
+// $(function() { });
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 35000,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+  direction: getDirection(),
+  on: {
+    resize: function resize() {
+      swiper.changeDirection(getDirection());
+    }
+  }
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 767 ? 'vertical' : 'horizontal';
+  return direction;
+}
 //# sourceMappingURL=all.js.map
