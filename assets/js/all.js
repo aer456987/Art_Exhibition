3d40a7af0b553ab04549af4d1a0f0ac9cc1e987d
Tori <aer456987@gmail.com>
2021-08-17 12:36:37 +0800

    Update 2021-08-17T04:36:34.890Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,39 +1,42 @@
 "use strict";
 
+// $(function() { });
 AOS.init({
   once: true
-}); // $(function() { });
-
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false
-  },
-  breakpoints: {
-    767: {
-      slidesPerView: 2
-    },
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    }
-  },
-  direction: getDirection(),
-  on: {
-    resize: function resize() {
-      swiper.changeDirection(getDirection());
-    }
-  }
 });
+var jsSwiper = document.querySelector('.js-swiper');
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 767 ? 'vertical' : 'horizontal';
-  return direction;
+if (jsSwiper) {
+  var getDirection = function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 767 ? 'vertical' : 'horizontal';
+    return direction;
+  };
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      767: {
+        slidesPerView: 2
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    direction: getDirection(),
+    on: {
+      resize: function resize() {
+        swiper.changeDirection(getDirection());
+      }
+    }
+  });
+  ;
 }
-
-;
 //# sourceMappingURL=all.js.map
